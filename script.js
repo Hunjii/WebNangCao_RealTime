4179dd78e9ca53839ea4cc6f80ff929fca269289
Hunji <Hungnmnm@gmail.com>
2020-08-25 07:52:35 +0700

    Add comment

--- script.js
+++ script.js
@@ -3,23 +3,29 @@ const messContainer = document.getElementById('message-container');
 const messInput = document.getElementById('message-input');
 const messForm = document.getElementById('send-container');
 
+// prompt lấy tên kết nối
 const name = prompt('Tên bạn là gì ?');
 
+// emit event thông tin người kết nối
 appendMessage('Bạn đã tham gia', '', 'left');
 socket.emit('new-user', name);
 
+// nhận thông tin chat
 socket.on('chat-message', (data) => {
   appendMessage(data.message, data.name, 'left');
 });
 
+// nhận thông tin người kết nối
 socket.on('user-connected', (data) => {
   appendMessage(`${data} đã kết nối`, 'Bot Chat', 'left');
 });
 
+// nhận thông tin người ngắt kết nối
 socket.on('user-disconnected', (data) => {
   appendMessage(`${data} đã thoát kết nối`, 'Bot Chat', 'left');
 });
 
+// event submit chat
 messForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const message = messInput.value;
@@ -28,6 +34,7 @@ messForm.addEventListener('submit', (e) => {
   messInput.value = '';
 });
 
+// Hàm tạo chat trên Html
 function appendMessage(message, name, align) {
   const messageElement = document.createElement('div');
   messageElement.style.background = '#fff';
--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const io = require('socket.io')(3000);
 
 const users = {};
 
+// Mở kết nối
 io.on('connection', (socket) => {
+  // event lấy thông tin người kết nối
   socket.on('new-user', (name) => {
     users[socket.id] = name;
     socket.broadcast.emit('user-connected', name);
   });
 
+  // event nhận chat
   socket.on('send-chat-message', (message) => {
     socket.broadcast.emit('chat-message', {
       message: message,
@@ -15,6 +18,7 @@ io.on('connection', (socket) => {
     });
   });
 
+  // event lấy thông tin người ngắt kết nối
   socket.on('disconnect', (name) => {
     socket.broadcast.emit('user-disconnected', users[socket.id]);
     delete users[socket.id];
